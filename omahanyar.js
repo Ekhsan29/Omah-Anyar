d4d63b29bef710fc02a1cea46ec2ec2bb95be2f6
Ekhsan29 <46285463+Ekhsan29@users.noreply.github.com>
2019-01-05 06:31:35 +0700

    Update omahanyar.js

--- omahanyar.js
+++ omahanyar.js
@@ -23,6 +23,13 @@ bot.on("message", async message => {
         message.channel.sendMessage("saya aktif!");
     }
     
+    if (message.content == "pagi") {
+        message.channel.sendMessage("`Pagi juga` ");
+    }
+    if (message.content == "Hai") {
+        message.channel.sendMessage("*Hai.... Halo... Hai...*");
+    }
+    
     if (message.author.equals(bot.user)) return;
     var dadu =["1&1", "1&2", "1&3", "1&4", "1&5", "1&6","2&1", "2&2", "2&3", "2&4", "2&5", "2&6","3&1", "3&2", "3&3", "3&4", "3&5", "3&6","4&1", "4&2", "4&3", "4&4", "4&5", "4&6", "5&1", "5&2", "5&3", "5&4", "5&5", "5&6","6&1", "6&2", "6&3", "6&4", "6&5", "6&6" ]
     var throwDadu = dadu[Math.floor(Math.random() *dadu.length)];
